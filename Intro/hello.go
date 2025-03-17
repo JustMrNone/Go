7023dbc45c64e09a95c49a7e95fb8229c32cafd8
Kamyar Bashar <kamyar.bashar@gmail.com>
2025-03-17 10:24:30 +0330

    inital commit for Go repo

---
+++ Intro/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Hello to all bitches in the world")
+	fmt.Println(quote.Go())
+}
---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module github.com/JustMrNone/Go/greetings
+
+go 1.23.6
