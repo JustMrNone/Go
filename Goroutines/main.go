ce9a81fc54358aa9660e284a712253d675ebfd7c
Kamyar Bashar <kamyar.bashar@gmail.com>
2025-03-27 12:48:02 +0330

    nan

--- Goroutines/main.go
+++ Goroutines/main.go
@@ -1,1 +1,36 @@
-//concurrency != parallel execution 
+//concurrency != parallel execution
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg = sync.WaitGroup{}
+
+var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
+var results = []string{}
+
+func main() {
+	t0 := time.Now()
+	for i := 0; i < len(dbData); i++ {
+		//we use a go keyword infront of a function that you want to run cuncurrently
+		wg.Add(1)
+		go dbCall(i)
+	}
+	wg.Wait()
+	fmt.Printf("\nTotal execution time: %v\n", time.Since(t0))
+	fmt.Printf("\n The results are %v", results)
+	fmt.Print("\n")
+}
+
+func dbCall(i int) {
+	var delay float32 = rand.Float32() * 2000
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	fmt.Println("The result from the database is:", dbData[i])
+	results = append(results, dbData[i])
+	wg.Done()
+}
